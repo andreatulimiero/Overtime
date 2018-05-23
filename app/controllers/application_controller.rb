b12aab0cf030a481b929e5e651f8550a7db16c75
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2018-05-23 10:20:46 +0200

    Fixed comment tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # FIX: Add this back
+  # FIXME: Add this back
   # protect_from_forgery with: :exception
 end
