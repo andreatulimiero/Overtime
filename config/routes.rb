0c2558a57b0f4a9c6b934ee2667263705f6078bf
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2018-05-23 10:18:54 +0200

    Fixed routing for comments and discussions, now they are "resources"

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resource :discussions do
-	  resource :comments
+  resources :discussions do
+	  resources :comments
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
