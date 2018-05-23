ed5006aa6b75cd619364a1feddb03f88c48cd4ae
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2018-05-23 10:19:05 +0200

    Basic create, show and index

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,9 +1,25 @@
 class DiscussionsController < ApplicationController
+    def index
+        @discussions = Discussion.all
+
+        render plain: @discussions.length
+    end
+
+    def show
+        @discussion = Discussion.find(params[:id])
+
+        render plain: @discussion
+    end
+
     def create
+        @discussion = Discussion.new(discussions_params)
+        @discussion.save
+
+        render plain: @discussion
     end
 
     private
         def discussions_params
-            params.require(:discussion).permit(:title, :body)
+            params.permit(:title, :body)
         end
 end
