2a21271bdf8a5fc9f4ebc20039e99dc8a7e4b1d8
Riccardo Bianchini <riccardo.bianchini96@gmail.com>
2018-05-24 22:37:27 +0200

    Add views for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,27 +1,31 @@
 class ArticlesController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
 
-    def index
-        @articles = Articles.all
+    def new
 
-        render :plain @articles.length
+    end
+
+    def index
+        @articles = Article.all
+
+        render plain: @articles.length
     end
 
     def show
-        @article = Articles.find params[:id]
+        @article = Article.find params[:id]
 
-        render :plain @article.title + @article.body
+        render plain: @article.title + @article.body
     end
 
     def create
-        @article = Articles.new articles_params
+        @article = Article.new articles_params
         if @article.save
             redirect_to @article
         else
             render 'new'
         end
 
-        render :plain @article.title + "created"
+        render plain: @article.title + "created"
     end
 
     def update
@@ -35,10 +39,10 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Articles.find params[:id]
+        @article = Article.find params[:id]
         @article.destroy
 
-        render :plain @article.title + "destroyed"
+        render plain: @article.title + "destroyed"
     end
 
     private 
