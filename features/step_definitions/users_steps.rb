870a2e3653cf1c9dec6947f5803e2031a7b8a68f
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2018-05-26 11:09:29 +0200

    Added steps to log in as another user

--- features/step_definitions/users_steps.rb
+++ features/step_definitions/users_steps.rb
@@ -4,6 +4,12 @@ Given("I am a logged in user") do
     login(@user.email, 'passpass')
 end  
 
+Given("I am another logged in user") do
+    @user = User.create!(:email => 'otheruser@user.com', :password => 'passpass')
+    @current_user = @user
+    login(@user.email, 'passpass')
+end
+
 module LoginSteps
     def login(email, password)
         visit('/users/sign_in')
@@ -12,13 +18,7 @@ module LoginSteps
         click_button('Log in')
     end
     def register(email, password)
-        visit('/users/sign_up')
-        fill_in('Email', :with => email)
-        fill_in('First name', :with => email)
-        fill_in('Last name', :with => email)
-        fill_in('Password', :with => password)
-        fill_in('Password confirmation', :with => password)
-        click_button('Sign up')
+        pass
     end
 end
   
