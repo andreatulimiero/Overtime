f97bf2c6db4f7cfce736fdc924af41b489193061
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2018-05-31 00:55:11 +0200

    Admin can now edit and delete any discussion

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -1,6 +1,14 @@
 module DiscussionsHelper
+    def can_see_edit_and_delete(discussion)
+        p discussion
+        if current_user.nil?
+            return false
+        end
+        discussion.user == current_user or current_user.admin
+    end
+
     def total_votes
         @discussion = Discussion.find(params[:id])
         @discussion.discussion_upvotes.count - @discussion.discussion_downvotes.count
-    end  
+    end
 end
