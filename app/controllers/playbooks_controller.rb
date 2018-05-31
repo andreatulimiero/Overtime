d8781f8a4b54fe0e4f23fdc8b7d5d0e58616e7f4
Riccardo Bianchini <riccardo.bianchini96@gmail.com>
2018-05-31 21:06:37 +0200

    Add controller playbooks

--- app/controllers/playbooks_controller.rb
+++ app/controllers/playbooks_controller.rb
@@ -1,2 +1,61 @@
 class PlaybooksController < ApplicationController
+    before_action :authenticate_user!
+
+    def new
+        @playbook = Playbook.new
+    end
+
+    def edit
+        @playbook = Playbook.find(params[:id])
+    end
+
+    def index
+        @playbooks = Playbook.all
+    end
+
+    def show
+        @playbook = Playbook.find params[:id]
+    end
+
+    def create
+        if current_user.admin?
+            @playbook = Playbook.new playooks_params
+            if @playbook.save
+                redirect_to @playbook
+            else
+                render 'new'
+            end
+        else
+            redirect_to root_path
+        end
+    end
+
+    def update
+        if current_user.admin?
+            @playbook = Playbook.find(params[:id])
+            @playbook.update(articles_params)
+          
+            redirect_to @playbook
+        else
+          redirect_to root_path
+        end
+    end
+
+    def destroy
+        if current_user.admin?
+            @playbook = Playbook.find(params[:id])
+            @playbook.destroy
+ 
+            redirect_to playbooks_path
+        else
+            redirect_to root_path
+        end
+    end
+
+    private 
+        def playbooks_params
+            params.require(:playbook).permit(:title, :body)
+        end
+end
+
 end
