64cfbb27e7bb0cc938986f3d6dcf1de629e6ec8c
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2018-06-02 01:32:56 +0200

    Email sending working

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,34 @@
 class Article < ActiveRecord::Base
+    require 'sendgrid-ruby'
+    include SendGrid
+    include Rails.application.routes.url_helpers
+
     validates :title, presence: true,
                     length: { minimum: 5 }
     belongs_to :user
     belongs_to :team
+    
+    after_create :after_creation_hook
+
+    def after_creation_hook
+        mail = Mail.new
+        mail.from = Email.new(email: 'tulimiero.andrea@gmail.com')
+        
+        personalization = Personalization.new
+        personalization.subject = 'A new article about ' + self.team.name + ' is out!'
+        self.team.users.each do |u|
+            p 'Adding ' + u.email
+            personalization.add_to(Email.new(email: u.email))
+        end
+        mail.add_personalization(personalization)
+        host = 'http://localhost:3000'
+        path = '/articles/' + self.id.to_s
+        msg = 'Check it out <a href="' + host + path + '">here</a>' 
+        mail.add_content(Content.new(type: 'text/html', value: msg))
+
+        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+        response = sg.client.mail._('send').post(request_body: mail.to_json)
+        p 'Sent email to  with code: ' + response.body
+        # TODO: Handle response error
+    end
 end
